a32a02c3b239e37aef45e757526e89a1a60476b8
Ryalian <ryalianese@gmail.com>
2014-11-03 01:35:01 -0500

    A cloud based image editing Android Application
    This is an android application that ask for an imge from local device
    and attach graphical component onto it. User can edit the image and
    upload it to the cloud side and pull it back after log in.
    
    This is as showcase of combination of transform, color rendering and
    ajax in Android platform. The compatibility of this application is from
    Android 2.3 to 4.4.2. The cloud side is build with PHP and mySQL.

---
+++ gen/edu/msu/cse/chenqiju/madhatter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.msu.cse.chenqiju.madhatter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/msu/cse/chenqiju/madhatter/ColorSelectActivity.java
@@ -0,0 +1,26 @@
+package edu.msu.cse.chenqiju.madhatter;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ColorSelectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_color_select);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.color_select, menu);
+		return true;
+	}
+
+    public void selectColor(int color) {
+
+        
+    }
+}
